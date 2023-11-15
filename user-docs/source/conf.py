6406d8ae42964976874bb55263e31e54b1f0a0ae
Stephan Heunis <s.heunis@fz-juelich.de>
2023-11-15 22:45:13 +0100

    basic sphinx documentation setup

---
+++ user-docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ABCDJ'
+copyright = '2023, Psychoinformatics Group, INM-7, FZJ'
+author = 'Psychoinformatics Group, INM-7, FZJ'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ user-docs/source/index.rst
@@ -0,0 +1,20 @@
+.. ABCDJ documentation master file, created by
+   sphinx-quickstart on Wed Nov 15 22:37:28 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ABCDJ's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
