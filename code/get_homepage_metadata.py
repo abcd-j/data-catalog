eb8945230685c322198facbb5df47474fe9e35d7
Stephan Heunis <s.heunis@fz-juelich.de>
2023-11-23 14:02:09 +0100

    updated scripts for core and tabby metadata extraction and translation to catalog schema

---
+++ code/get_homepage_metadata.py
@@ -0,0 +1,62 @@
+from argparse import ArgumentParser
+import json
+from pathlib import Path
+
+from datalad_catalog.extractors import catalog_core
+from datalad_next.constraints.dataset import EnsureDataset
+
+from get_tabby_metadata import get_metadata
+
+if __name__ == "__main__":
+
+    repo_path = Path(__file__).resolve().parent.parent
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "dataset_path", type=str, help="Path to the datalad dataset",
+    )
+    parser.add_argument("--add-to-catalog", action="store_true")
+    args = parser.parse_args()
+
+    ds = EnsureDataset(
+        installed=True, purpose="extract core metadata", require_id=True
+    )(args.dataset_path).ds
+
+    # first get core metadata
+    core_record = catalog_core.get_catalog_metadata(ds)
+    # then get tabby metadata
+    tabby_path = tabby_path = Path(args.dataset_path) / '.datalad/tabby/self/dataset@tby-abcdjv0.tsv'
+    tabby_record = get_metadata(ds, tabby_path)
+
+    if args.add_to_catalog:
+        from datalad.api import  (
+            catalog_add,
+            catalog_set,
+        )
+
+        catalog_dir = repo_path / 'docs'
+
+        # Add core metadata to the catalog
+        catalog_add(
+            catalog=catalog_dir,
+            metadata=json.dumps(core_record),
+            config_file = repo_path / 'inputs' / 'superds-config.json',
+        )
+
+        # Add tabby metadata to the catalog
+        catalog_add(
+            catalog=catalog_dir,
+            metadata=json.dumps(core_record),
+            config_file = repo_path / 'inputs' / 'superds-config.json',
+        )
+        catalog_set(
+            catalog=catalog_dir,
+            property="home",
+            dataset_id=ds.id,
+            dataset_version=ds.repo.get_hexsha(),
+            reckless="overwrite",
+        )
+
+    print(json.dumps(core_record))
+    print("\n\n")
+    print(json.dumps(tabby_record))
---
+++ code/get_tabby_metadata.py
@@ -0,0 +1,61 @@
+from argparse import ArgumentParser
+import json
+from pathlib import Path
+from pyld import jsonld
+
+from datalad_catalog.schema_utils import (
+    get_metadata_item,
+)
+from datalad_next.constraints.dataset import EnsureDataset
+from datalad_tabby.io import load_tabby
+
+from utils import (
+    process_funding,
+    process_authors,
+    process_keywords,
+    CAT_CONTEXT,
+)
+
+def get_metadata(ds, tabby_path):
+    repo_path = Path(__file__).resolve().parent.parent
+    home_record = load_tabby(
+        src=tabby_path,
+        single=True,
+        jsonld=True,
+        recursive=True,
+        cpaths=[repo_path / 'inputs'],
+    )
+    expanded = jsonld.expand(home_record)
+    compacted = jsonld.compact(home_record, ctx=CAT_CONTEXT)
+    # Use catalog schema_utils to get base structure of metadata item
+    meta_item = get_metadata_item(
+        item_type='dataset',
+        dataset_id=ds.id,
+        dataset_version=ds.repo.get_hexsha(),
+        source_name="tabby",
+        source_version="0.1.0",
+    )
+    # note: this becomes catalog page title, so title fits better
+    meta_item["name"] = compacted.get("title")
+    meta_item["description"] = compacted.get("description")
+    meta_item["doi"] = compacted.get("doi")
+    meta_item["authors"] = process_authors(compacted.get("authors"))
+    meta_item["keywords"] = process_keywords(compacted.get("keywords"))
+    meta_item["funding"] = process_funding(compacted.get("funding"))
+    return meta_item
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "dataset_path", type=Path, help="Path to the datalad dataset"
+    )
+    args = parser.parse_args()
+    ds = EnsureDataset(
+        installed=True, purpose="extract tabby metadata", require_id=True
+    )(args.dataset_path).ds
+
+    tabby_path = Path(args.dataset_path) / '.datalad/tabby/self/dataset@tby-abcdjv0.tsv'
+    meta_item = get_metadata(ds, tabby_path)
+    print(json.dumps(meta_item))
